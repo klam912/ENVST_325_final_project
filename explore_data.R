3730c2a27bd64268c75a7c3bf09faeb9fbb6c987
klam912 <lamkenny912@gmail.com>
2026-04-30 10:38:57 -0400

    add initial data exploratory R script

---
+++ explore_data.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+data_centers <- read.csv("Desktop/ENVST325/final_project/ENVST_325_final_project/data_repo/im3_open_source_data_center_atlas_v2026.02.09/im3_open_source_data_center_atlas_v2026.02.09.csv")
+eji <- read.csv("Desktop/ENVST325/final_project/ENVST_325_final_project/data_repo/EJI_2024_United_States_CSV/EJI_2024_United_States.csv")
+nih <- read.csv("Desktop/ENVST325/final_project/ENVST_325_final_project/data_repo/NIH/HDPulse_data_export.csv")
+
